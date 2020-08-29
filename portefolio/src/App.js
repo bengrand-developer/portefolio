b414695088cc8d2f0d0fbef7567c943c276d780e
GRAND <bengrand.pro@gmail.com>
2020-08-29 11:30:27 +0200

    add navbar file

--- portefolio/src/App.js
+++ portefolio/src/App.js
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      {/* <Switch>
+          <Route exact path="/" component={Accueil} />
+          <Route path="/competences" component={Skills} />
+          <Route path="/experiences" component={Experiences} />
+          <Route path="/portefolio" component={Portefolio} />
+          <Route path="/contact" component={Contact} />
+        </Switch> */}
     </div>
   );
 }
