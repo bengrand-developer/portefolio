dca70ece6f5d0161bc9ce169ceae807cd0b1169c
GRAND <bengrand.pro@gmail.com>
2020-10-20 16:23:49 +0200

    chang env

--- portefolio/server/server.js
+++ portefolio/server/server.js
@@ -1,7 +1,7 @@
 const path = require ('path');
 const express= require ('express');
 const app = express();
-const port= process.env.PORT || 3000;
+const port= 3000;
 
 
 const publicPath= path.join(_dirname, '..', 'public');
