a90f0fd5b8966c3c27085d4fbd3cd8ebbe2a6e5e
GRAND <bengrand.pro@gmail.com>
2020-10-24 16:37:07 +0200

    responsive navbar test

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -13,38 +13,44 @@ const Navbar = () => {
         largeMenuClassName="large-menu-classname"
         smallMenuClassName="small-menu-classname"
         menu={
-    <header className="Header">
-      <div className="Navbar-container">
-        <div className="navbar-logo">
-          <img src={devIcon} alt="dev-logo" className="devIcon" />
-          <div className="all-title">
-            <h3 className="navbar-title">Benoit Grand</h3>
-            <h5 className="navbar-subtitle">Développeur Web</h5>
-          </div>
-        </div>
-        <p className="Navbar-item">
-          <NavLink exact to="/">
-            Accueil
-          </NavLink>
-        </p>
+          <ul>
+          <li>Item 1</li>
+          <li>Item 2</li>
+          <li>Item 3</li>
+          <li>Item 4</li>
+        </ul>
+    // <header className="Header">
+    //   <div className="Navbar-container">
+    //     <div className="navbar-logo">
+    //       <img src={devIcon} alt="dev-logo" className="devIcon" />
+    //       <div className="all-title">
+    //         <h3 className="navbar-title">Benoit Grand</h3>
+    //         <h5 className="navbar-subtitle">Développeur Web</h5>
+    //       </div>
+    //     </div>
+    //     <p className="Navbar-item">
+    //       <NavLink exact to="/">
+    //         Accueil
+    //       </NavLink>
+    //     </p>
 
-        <p className="Navbar-item">
-          <NavLink to="/competences">Compétences</NavLink>
-        </p>
+    //     <p className="Navbar-item">
+    //       <NavLink to="/competences">Compétences</NavLink>
+    //     </p>
 
-        <p className="Navbar-item">
-          <NavLink to="/experiences">Experiences</NavLink>
-        </p>
+    //     <p className="Navbar-item">
+    //       <NavLink to="/experiences">Experiences</NavLink>
+    //     </p>
 
-        <p className="Navbar-item">
-          <NavLink to="/realisations">Réalisations</NavLink>
-        </p>
+    //     <p className="Navbar-item">
+    //       <NavLink to="/realisations">Réalisations</NavLink>
+    //     </p>
 
-        <p className="Navbar-item">
-          <NavLink to="/contact">Contact</NavLink>
-        </p>
-      </div>
-    </header>
+    //     <p className="Navbar-item">
+    //       <NavLink to="/contact">Contact</NavLink>
+    //     </p>
+    //   </div>
+    // </header>
         }/>
   );
 };
